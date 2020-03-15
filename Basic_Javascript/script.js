f8b77e4e9c435ca563b1fdac0bfc3a45b963b795
Souvikcse <sayanwinz@gmail.com>
2020-03-15 23:47:03 +0530

    Intro to Javascript
    Signed-off-by: Souvikcse <sayanwinz@gmail.com>

---
+++ Basic_Javascript/script.js
@@ -0,0 +1,6 @@
+var fn = prompt("Enter your first name")
+var ln = prompt("Enter your last name")
+var age=  prompt("What is your age")
+
+console.log("Your full name is " + fn + " "+ ln)
+console.log("You are "+ age + " years old")
