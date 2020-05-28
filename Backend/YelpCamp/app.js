4c341df2591f2b2240e79b05fde5ddc395dd4db8
Souvikcse <sayanwinz@gmail.com>
2020-05-28 16:37:04 +0530

    Version 1.0
    Signed-off-by: Souvikcse <sayanwinz@gmail.com>

--- Backend/YelpCamp/app.js
+++ Backend/YelpCamp/app.js
@@ -1,15 +1,33 @@
 var express = require('express')
 var app = express()
+var bodyParser = require('body-parser');
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended:true}));
+var campgrounds = [ {name: "Little Boy", image: "https://images.freeimages.com/images/small-previews/c23/hello-3-1564990.jpg"},
+                    {name: "Delicate Arch", image: "https://images.freeimages.com/images/small-previews/1f8/delicate-arch-1-1391746.jpg"},
+                    {name: "Rain on sea", image: "https://images.freeimages.com/images/small-previews/3a6/rain-on-sea-ii-1368899.jpg"},
+                    {name: "Little Boy", image: "https://images.freeimages.com/images/small-previews/c23/hello-3-1564990.jpg"},
+                    {name: "Delicate Arch", image: "https://images.freeimages.com/images/small-previews/1f8/delicate-arch-1-1391746.jpg"},
+                    {name: "Rain on sea", image: "https://images.freeimages.com/images/small-previews/3a6/rain-on-sea-ii-1368899.jpg"},
+                    {name: "Little Boy", image: "https://images.freeimages.com/images/small-previews/c23/hello-3-1564990.jpg"},
+                    {name: "Delicate Arch", image: "https://images.freeimages.com/images/small-previews/1f8/delicate-arch-1-1391746.jpg"},
+                    {name: "Rain on sea", image: "https://images.freeimages.com/images/small-previews/3a6/rain-on-sea-ii-1368899.jpg"} ];
 app.get("/", function(req, res){
     res.render("landing");
 });
 app.get('/campgrounds', function(req, res){
-    var campgrounds = [{name: "Little Boy", image: "https://images.freeimages.com/images/small-previews/c23/hello-3-1564990.jpg"},
-                       {name: "Delicate Arch", image: "https://images.freeimages.com/images/small-previews/1f8/delicate-arch-1-1391746.jpg"} ];
     res.render("campgrounds", {campgrounds: campgrounds});
 });
+app.post('/campgrounds', function(req, res){
+    var name = req.body.name;
+    var image = req.body.image;
+    campgrounds.push({name: name, image: image});
+    res.redirect("/campgrounds");
+});
+app.get('/campgrounds/new', function(req, res){
+    res.render("new");
+})
 
 app.listen(3000, function(){
     console.log("YelpCamp Server started");
